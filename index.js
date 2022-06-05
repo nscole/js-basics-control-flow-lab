6674ada5c7cf9de2678fbf3eff2f159b03fa67d8
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 14:11:20 +1000

    Passing 1st test of 3

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  if (feet <= 400){
+    return 'This one is on me!';
+  }
+  else if (feet > 2500) {
+    return 'No can do.'
+  }
+  else if (feet > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+  
 }
 
 function ternaryCheckCity(){
@@ -7,5 +18,5 @@ function ternaryCheckCity(){
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  // Write your code here! 
 }
