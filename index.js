d1b24b993f04589677c1e336d2fdf2611d1bb29c
Naomi Cole <naomisarahcole@gmail.com>
2022-06-05 14:41:34 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -22,28 +22,35 @@ function ternaryCheckCity(city){
   }
 }
 
-/*
-  describe('ternaryCheckCity()', function () {
-    it('returns "Ok, sounds good." when the city is NYC', function () {
-      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-    });
 
-    it('should return "No go." if the destination city is not NYC', function () {
-      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-    });
-  });
-*/
-
-
-
-
-
-
-
-
-
-
-
-function switchOnCharmFromTip(){
-  // Write your code here! 
+function switchOnCharmFromTip(tip){
+  let message;
+ switch (tip) {
+  case 'generous':
+    message = "Thank you so much.";
+    break;
+  case 'not as generous':
+    message = "Thank you.";
+    break;
+  case 'thanks for everything' :
+    message = "Bye.";
+    break;
+ } 
+ return message;
 }
+
+
+/*
+ describe('switchOnCharmFromTip()', function () {
+    it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
+
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
+
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    });
+*/
